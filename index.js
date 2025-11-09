dc3fd2f6d5163f79b4f54b1f422d4775ded948c5
Shohel-Raj <shohel87.dev@gmail.com>
2025-11-10 02:11:33 +0600

    Mongodb connected

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-
-
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // Sample route
-app.get("/", (req, res) => {
-  res.send("MovieMaster Pro Backend is running!");
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    app.get("/", (req, res) => {
+      res.send("MovieMaster Pro Backend is running!");
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Start server
 app.listen(PORT, () => {
